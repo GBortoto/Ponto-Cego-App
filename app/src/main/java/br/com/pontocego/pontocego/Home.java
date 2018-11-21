8e1df1f95b4b1110980ee77b87af6cad1d1e0a2b
ThiagoSMB <thiagosmb@gmail.com>
2018-11-21 03:12:18 -0200

    Primeiro Commitapp/src/main/AndroidManifest.xml

--- app/src/main/java/br/com/pontocego/pontocego/Home.java
+++ app/src/main/java/br/com/pontocego/pontocego/Home.java
@@ -1,26 +1,68 @@
 package br.com.pontocego.pontocego;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.speech.RecognizerIntent;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
 
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
+
 public class Home extends AppCompatActivity {
 
-    //private TextView txvResult;
+    private FusedLocationProviderClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        //txvResult = (TextView) findViewById(R.id.txvResult);
+
+        requestPermission();
+
+        client = LocationServices.getFusedLocationProviderClient(this);
+
+        Button button = findViewById(R.id.getLocation);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (ActivityCompat.checkSelfPermission( Home.this, ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+                    return;
+                }
+                client.getLastLocation().addOnSuccessListener(Home.this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+
+                        if (location!=null){
+                            TextView textViewLat = findViewById(R.id.latitude);
+                            textViewLat.setText(String.valueOf(location.getLatitude()));
+                            TextView textViewLong = findViewById(R.id.longitude);
+                            textViewLong.setText(String.valueOf(location.getLongitude()));
+                        }
+                    }
+                });
+            }
+        });
+    }
+
+    private void requestPermission(){
+        ActivityCompat.requestPermissions(this, new String[]{ACCESS_FINE_LOCATION}, 1);
     }
 
     public void getSpeechInput(View view) {
